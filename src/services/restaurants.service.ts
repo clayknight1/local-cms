995a3e6e440e3ca344f10a4bc9081083c2897597
Clay Knight <clayknight@rocketmail.com>
2026-03-06 09:28:32 -0800

    remove getRestaraumnts() form service

--- src/services/restaurants.service.ts
+++ src/services/restaurants.service.ts
@@ -1,26 +1,6 @@
 import { supabase } from '../lib/supabase';
 import type { Business, BusinessInsert } from '../types';
 
-// export async function getRestaurants(): Promise<Business[]> {
-//   const { data, error } = await supabase
-//     .from('businesses')
-//     .select(`*`)
-//     .eq('tenant_id', 1)
-//     .in('business_type', ['Restaurant', 'Cafe'])
-//     .eq('is_active', true)
-//     .eq('is_archived', false);
-
-//   if (error) {
-//     console.error(error);
-//     throw error;
-//   }
-//   if (!data) {
-//     return [];
-//   }
-
-//   return data;
-// }
-
 export async function getBusinessById(id: number): Promise<Business | null> {
   const { data, error } = await supabase
     .from('businesses')
